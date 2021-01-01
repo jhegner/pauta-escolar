0a9fa5fc5011bb402f2767cabc0441e29a5f3da6
Jidlafe Hegner <jidlafehegner@gmail.com>
2021-01-01 13:40:27 -0300

    feat: Pagina de login e dependencias com o bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,55 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import {Button, Container, Form, Jumbotron, NavLink} from "react-bootstrap";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Login() {
+    return (
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Container fluid="lg">
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+            <Head>
+                <title>Pauta Escolar | Controle das notas dos alunos</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <Jumbotron fluid>
+                <Container>
+                    <h1># Pauta Escolar</h1>
+                    <p>Controle das notas dos alunos</p>
+                </Container>
+            </Jumbotron>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <main>
+                <Container fluid="sm">
+                    <Form>
+                        <Form.Group controlId="formBasicID">
+                            <Form.Label>ID ou Telefone</Form.Label>
+                            <Form.Control type="email" placeholder="ID de acesso ou telefone" />
+                            <Form.Text className="text-muted">
+                                Nunca compartilhe seus dados de acesso com ninguém.
+                            </Form.Text>
+                        </Form.Group>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password" placeholder="Senha de acesso" />
+                        </Form.Group>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+                        <Button variant="primary" type="submit">
+                            Entrar
+                        </Button>
+                        <NavLink>
+                            Esqueci meu usuário ou senha
+                        </NavLink>
+                    </Form>
+                </Container>
+            </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+            <footer className={styles.footer}>
+                Powered by Nercode Tecnologia
+            </footer>
+
+        </Container>
+    )
 }
